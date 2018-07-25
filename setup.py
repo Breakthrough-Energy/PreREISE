94f41e24ca9a10d1308c8bd03d1bdd7e34a32be1
kasparm <10716532+kasparm@users.noreply.github.com>
2018-07-25 15:03:11 -0700

    Add setup and adjut __init__.py to make the package user friendly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='prereise',
+      version='0.1',
+      description='Create and run an energy scenario',
+      url='https://github.com/intvenlab/PreREISE',
+      author='Kaspar Mueller',
+      author_email='kmueller@intven.com',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
