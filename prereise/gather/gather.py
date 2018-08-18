b15dcd360778c66c103e9337f68aa33d06859bb4
kasparm <10716532+kasparm@users.noreply.github.com>
2018-08-17 22:01:03 -0700

    wind gather is working. Comments need to be adapted and further testing need to be done

--- prereise/gather/gather.py
+++ prereise/gather/gather.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot as plt # Maybe not needed
+#import matplotlib.pyplot as plt # Maybe not needed
+from math import *
 import pandas as pd
 import requests
 import sys
@@ -14,13 +15,23 @@ from westernintnet.westernintnet import WesternIntNet
 from inspect import formatannotationrelativeto
 win = WesternIntNet()
 
-# Retrieve all siteID's for wind farms in states part of the western interconnect network
+def _greatCircleDistance(lat1, lon1, lat2, lon2):
+    R = 6368
+    def haversin(x):
+        return sin(x/2)**2
+    return R*2 * asin(sqrt(
+          haversin(lat2-lat1) +
+          cos(lat1) * cos(lat2) * haversin(lon2-lon1)))
+
 def get_all_NREL_siteID_for_states(states_list):
+    """Retrieve all siteID's for wind farms in states in states_list.
+    Returns Pandas DataFrame with columns ['site_id', 'lat', 'lon', 'capacity', 'capacity_factor'].
+    """
     nrel_sites = None
-    
+
     for state in states_list:
         coords = np.column_stack((states[state]['lons'],states[state]['lats']))
-        
+
         # Prepare coordinates for call
         # Convert into string format
         out_tot = []
@@ -42,65 +53,130 @@ def get_all_NREL_siteID_for_states(states_list):
     return nrel_sites
 
 # Find NREL siteID closest to wind farms
-def find_NREL_siteID_closest_to_windfarm(nrel_sites, windfarm_subID):
-    closest_NREL_siteID = pd.Series(index = windfarm_subID.index)
+def find_NREL_siteID_closest_to_windfarm(nrel_sites, wind_farm_bus):
+    """Takes Pandas DataFrame nrel_sites that needs the columns ['site_id', 'lat', 'lon','capacity']
+    and wind_farm_bus as Pandas DataFrame with the following structure: ['plantID'(index), 'lat', 'lon'].
+    The order of the columns plantID(index), lat and lon is important.
+    Returns Pandas Series with plantID as index, siteID and capacity as value.
+    """
+    closest_NREL_siteID = pd.DataFrame(index = wind_farm_bus.index,columns = ['siteID', 'capacity'])
     # Iterate trough wind farms and find closest NREL site
-    for i,row in enumerate(windfarm_subID.itertuples()): 
-        r_sq = (nrel_sites['lat']-row[1])*(nrel_sites['lat']-row[1]) + (nrel_sites['lon']-row[2])*(nrel_sites['lon']-row[2])
-        closest_NREL_siteID.iloc[i] = int(nrel_sites['site_id'][r_sq == r_sq.min()].values[0])
-    closest_NREL_siteID=closest_NREL_siteID.astype(int)
-    closest_NREL_siteID.index.name = 'subID'
-    closest_NREL_siteID.name = 'siteID'
+    for i,row in enumerate(wind_farm_bus.itertuples()):
+        dist = nrel_sites.apply(lambda row_sites : _greatCircleDistance(row_sites['lat'], row_sites['lon'],row[1],row[2]),axis=1)
+        #r_sq = (nrel_sites['lat']-row[1])*(nrel_sites['lat']-row[1]) + (nrel_sites['lon']-row[2])*(nrel_sites['lon']-row[2])
+        print(dist.min())
+        closest_NREL_siteID.iloc[i].siteID = (nrel_sites['site_id'][dist == dist.min()].values[0])
+        closest_NREL_siteID.iloc[i].capacity = (nrel_sites['capacity'][dist == dist.min()].values[0])
+    closest_NREL_siteID.index.name = 'plantID'
+    closest_NREL_siteID.siteID = closest_NREL_siteID.siteID.astype(int)
+    closest_NREL_siteID.capacity = closest_NREL_siteID.capacity.astype(float)
     return closest_NREL_siteID
 
-def get_data_from_NREL_server(siteIDs):
-    # Create data frame for power and wind speed
-    data_start = pd.Timestamp('2010-01-01')
-    data_end = pd.Timestamp('2012-12-31 23:55:00')
-    data_range = pd.date_range(data_start, data_end,freq = '5min')
-    NREL_power = pd.DataFrame(index=data_range,columns=siteIDs)
-    NREL_windspeed = pd.DataFrame(index=data_range,columns=sitesIDs)
-    
+def get_data_from_NREL_server(siteIDs, data_range):
+    """Takes Pandas DataFrame with siteIDs and capacity as values. It also takes
+    the pandas data_range as input. freq needs to be 5min.
+    Returns a 2D dict containing Pandas DataFrame with index date
+    and columns power and wind_speed. The data is normalized using the capacity.
+    The 1. key is the month like '2010-10'
+    The 2. key is the siteID like 121409
+    So the class site_dict['2012-12'][121409] returns the DataFrame.
+    """
     # Retrieving data from NREL server
     utc = True
     leap_day = True
+    # We use a dict because the output is a tensor
+    # 1: siteIDs 2: date(month) 3: attribute(power, wind_speed)
     sites_dict = {}
-    for y in data_range.year.unique():
-        for m in range(1,13):
+    # Use helper DataFrame to specifie download interval
+    helper_df = pd.DataFrame(index=data_range)
+
+    for y in helper_df.index.year.unique():
+        for m in helper_df[str(y)].index.month.unique():
             if(m<10):
                 month = str(y) + '-0' + str(m)
             else:
                 month = str(y) + '-' + str(m)
             sites_dict[month] = {}
-            start = NREL_power[month].index[0]
-            end = NREL_power[month].index[-1]     
+            start = helper_df[month].index[0]
+            end = helper_df[month].index[-1]
             attributes = ["power", "wind_speed"]
-            for site_id in siteIDs:
-                print('Call ' + str(site_id) + ' ' + str(start) + ' ' + str(end))
-                sites_dict[month][site_id] = get_nc_data_from_url(WTK_URL+"/met", site_id, start, end, attributes, utc=utc,leap_day=leap_day)
+            #for site_id in siteIDs.iloc[0:1].siteID:
+            #    print('Call ' + str(site_id) + ' ' + str(start) + ' ' + str(end))
+            #    sites_dict[month][site_id] = get_nc_data_from_url(WTK_URL+"/met", site_id, start, end, attributes, utc=utc,leap_day=leap_day)
+            for row in siteIDs.drop_duplicates(subset='siteID').itertuples():
+                site_id_i = row[1]
+                capacity_i = row[2]
+                print('Call ' + str(site_id_i) + ' ' + str(start) + ' ' + str(end))
+                sites_dict[month][site_id_i] = get_nc_data_from_url(WTK_URL+"/met", site_id_i, start, end, attributes, utc=utc,leap_day=leap_day)/capacity_i
     print('Done retrieving data from NREL server')
     return sites_dict
 
-def save_sites_dict_to_csv(data):
-# Save data in csv file
-    for y in data_range.year.unique():
-        for m in range(1,13):
-            if(m<10):
-                month = str(y) + '-0' + str(m)
-            else:
-                month = str(y) + '-' + str(m)
-            NREL_power.loc[month,site_id] = sites_dict[month][site_id]['power'].values
-            NREL_windspeed.loc[month,site_id] = sites_dict[month][site_id]['wind_speed'].values
-    NREL_power.to_csv('WIN-NREL2010-2012_power.csv')
-    NREL_windspeed.to_csv('WIN-NREL2010-2012_windspeed.csv')
+def dict_to_DataFrame(data, data_range, closest_NREL_siteID):
+    """Input the 2D dict containing Pandas DataFrame with index date
+    and columns power and wind_speed. It also takes the pandas data_range of
+    the data and the closest_NREL_siteID.
+
+    The 1. key is the month like '2010-10'
+    The 2. key is the siteID like 121409
+
+    Returns two DataFrames, one for power and one for wind speed
+
+    """
+    NREL_power = pd.DataFrame(index=data_range,columns=closest_NREL_siteID['siteID'].drop_duplicates(),dtype=float)
+    NREL_windspeed = pd.DataFrame(index=data_range,columns=closest_NREL_siteID['siteID'].drop_duplicates(),dtype=float)
+
+    for month in data:
+        print(month)
+        for siteID in data[month]:
+            print(siteID)
+            NREL_power.loc[month,siteID] = data[month][siteID]['power'].values
+            NREL_windspeed.loc[month,siteID] = data[month][siteID]['wind_speed'].values
+    return [NREL_power, NREL_windspeed]
+
+def scale_and_save_power_for_matpower(NREL_power, wind_farm_bus, closest_NREL_siteID):
+    """ Input data in 2D dict format containing normalized wind power time series
+    as well as wind_farm_bus containing information of the relation between siteID
+    and plantID(index in genbus) as well as Pmax of the plant in order to scale
+    the wind power.
+    """
+    wind_farm_power = pd.DataFrame(index=NREL_power.index,columns=wind_farm_bus.index.values)
+    for plantID, Pmax in wind_farm_bus['Pmax'].iteritems():
+        siteID = closest_NREL_siteID.loc[plantID,'siteID']
+        wind_farm_power[plantID] = NREL_power[siteID]*Pmax
+
+    wind_farm_power_series_hourly = wind_farm_power.resample('H').mean()
+    numb_farms = wind_farm_power_series_hourly.shape[1]
+    mp_power = []
+    mp_index = []
+    mp_plantID = []
+    for i,row in enumerate(wind_farm_power_series_hourly.itertuples()):
+        mp_index = mp_index + [i+1] * numb_farms
+        mp_plantID = wind_farm_bus.index.values.tolist() + mp_plantID
+        mp_power = mp_power + list(row[1:])
+
+    mp_wind_input = pd.DataFrame(index=mp_index,columns = ['plantID','power'])
+    mp_wind_input['plantID'] = mp_plantID
+    mp_wind_input['power'] = mp_power
+    mp_wind_input.index.name = 'period_hour'
+    return mp_wind_input
 
 def get_and_save_NREL_data():
-    
-    all_siteID_NREL  = get_NREL_siteID_for_states(WI.states_abbrev)
+
+    all_siteID_NREL  = get_all_NREL_siteID_for_states(['WA'])#(WI.states_abbrev)
     #Pandas DataFrame with subID of wind farm as index and with column label lat and lon
-    windfarm_subID = WI.sub.loc[WI.bus2sub.subID[WI.genbus.groupby('type').get_group('wind').busID],['lat','lon']].drop_duplicates()
+    #windfarm_subID = win.sub.loc[win.bus2sub.subID[win.genbus.groupby('type').get_group('wind').busID],['lat','lon']].drop_duplicates()
+    wind_farm_bus = win.genbus.groupby('type').get_group('wind')
     #returns map between subID (index) and NREL siteID (column)
-    closest_NREL_siteID = find_NREL_siteID_closest_to_windfarm(all_siteID_NREL, windfarm_subID)
-    data = get_data_from_NREL_server(closest_NREL_siteID)
-    save_sites_dict_to_csvL(data)
-    
+    closest_NREL_siteID = find_NREL_siteID_closest_to_windfarm(all_siteID_NREL, wind_farm_bus[['lat','lon']])
+    # Create data frame for power and wind speed
+    data_start = pd.Timestamp('2010-01-01')
+    data_end = pd.Timestamp('2010-01-01 23:55:00')
+    #data_end = pd.Timestamp('2012-12-31 23:55:00')
+    # The server provides data in 5min intervals
+    data_range = pd.date_range(data_start, data_end,freq = '5min')
+    data = get_data_from_NREL_server(closest_NREL_siteID, data_range)
+
+    [power,wind_speed] = dict_to_DataFrame(data, data_range, closest_NREL_siteID)
+    mp = scale_and_save_power_for_matpower(power, wind_farm_bus, closest_NREL_siteID)
+    mp.to_csv('PowerForMatPowerImport.csv')
+    #save_sites_dict_to_csvL(data)
