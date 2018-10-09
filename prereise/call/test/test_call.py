ab92f0734c47d5720f2fd4398a2f126e8a645913
kasparm <10716532+kasparm@users.noreply.github.com>
2018-10-09 16:27:39 -0700

    style: format file

--- prereise/call/test/test_call.py
+++ prereise/call/test/test_call.py
@@ -1,10 +1,12 @@
-import sys
 import os
+import sys
 import unittest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import call
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 def test():
     call.launch_scenario_performance('western_scenarioUnitTest02', 16)
