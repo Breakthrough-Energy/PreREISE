e91df1a37f65b8a20297d8a6ccae2e890a2ed59b
Ben Rouillé d'Orfeuil <brdo@intven.com>
2018-10-10 11:18:23 -0700

    feat: Add inplace option

--- prereise/gather/winddata/rap/impute.py
+++ prereise/gather/winddata/rap/impute.py
@@ -5,24 +5,29 @@ from tqdm import tqdm
 from helpers import get_power
 
 
-def simple_imputation(data, wind_farm):
+def simple(data, wind_farm, inplace=True):
     """Impute missing data using a simple procedure. For each missing entry,
-       the extrema of the U and V components of the wind speed of all non
-       missing entries that have the same location, same month, same hour
-       are first found for each missing entry. Then, a U and V value are
-       randomly generated between the respective derived ranges.
+    the extrema of the U and V components of the wind speed of all non missing
+    entries that have the same location, same month, same hour are first found
+    for each missing entry. Then, a U and V value are randomly generated
+    between the respective derived ranges.
 
-       :param data: pandas DataFrame as returned by
-       py:method:`rap.retrieve_data`.
-       :param wind_farm: pandas DataFrame of wind farms
-       :return: pandas DataFrame with missing entries imputed.
+    :param data: pandas DataFrame as returned by py:method:`rap.retrieve_data`.
+    :param wind_farm: pandas DataFrame of wind farms
+    :param inplace: should the
+    :return: pandas DataFrame with missing entries imputed.
     """
 
+    if inplace:
+        data_impute = data
+    else:
+        data_impute = data.copy()
+
     # Locate missing data
-    to_impute = data[data.U.isna()].index.values
+    to_impute = data[data.U.isna()].index
 
     # Timestamp of all entries in dataframe
-    dates = pd.DatetimeIndex(data['ts'].values)
+    dates = pd.DatetimeIndex(data.index.values)
 
     n_target = len(wind_farm)
     for i, j in tqdm(enumerate(to_impute), total=len(to_impute)):
@@ -31,7 +36,8 @@ def simple_imputation(data, wind_farm):
             month = dates[j].month
             day = dates[j].day
             hour = dates[j].hour
-            select = data[(dates.month == month) &
+            select = data[(dates.year == year) &
+                          (dates.month == month) &
                           (dates.hour == hour) &
                           (data.Pout != -99)]
 
@@ -40,13 +46,11 @@ def simple_imputation(data, wind_farm):
 
         minU, maxU = select_plant['U'].min(), select_plant['U'].max()
         minV, maxV = select_plant['V'].min(), select_plant['V'].max()
-        data.at[j, 'U'] = minU + (maxU - minU) * np.random.random()
-        data.at[j, 'V'] = minV + (maxV - minV) * np.random.random()
+        data_impute.at[j, 'U'] = minU + (maxU - minU) * np.random.random()
+        data_impute.at[j, 'V'] = minV + (maxV - minV) * np.random.random()
         wspd = np.sqrt(data.loc[j].U**2 + data.loc[j].V**2)
         capacity = wind_farm.loc[k].GenMWMax
-        data.at[j, 'Pout'] = get_power(wspd, 'IEC class 2') * capacity
+        data_impute.at[j, 'Pout'] = get_power(wspd, 'IEC class 2') * capacity
 
-    # Write output in csv file
-    name = "western_wind_output_2016_ts.csv"
-    data.to_csv(name, header=None, index=False,
-                columns=['tsID', 'plantID', 'Pout'])
+    if not inplace:
+        return data_impute
