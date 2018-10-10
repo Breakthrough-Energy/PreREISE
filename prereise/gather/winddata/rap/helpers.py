1b06a5007ab26557405009a05eae39e8ffaa2a74
Ben Rouillé d'Orfeuil <brdo@intven.com>
2018-10-10 11:17:10 -0700

    feat: Add to_reise function

--- prereise/gather/winddata/rap/helpers.py
+++ prereise/gather/winddata/rap/helpers.py
@@ -61,3 +61,26 @@ def get_power(wspd, turbine):
     return np.interp(wspd,
                      PowerCurves[turbine][match].index.values,
                      PowerCurves[turbine][match].values)
+
+
+def to_reise(data):
+    """Format data for REISE.
+
+    :param data: pandas DataFrame as returned by py:method:`rap.retrieve_data`.
+    :return: pandas DataFrame formated for REISE.
+    """
+    ts = data['ts'].unique()
+    plantID = data[data.tsID == 1].plantID.values
+
+    for i in range(1,max(data.tsID)+1):
+        data_tmp = pd.DataFrame({'Pout': data[data.tsID == i].Pout.values},
+                                index=plantID)
+        if i == 1:
+            dataT = data_tmp.T
+        else:
+            dataT = dataT.append(data_tmp.T, sort=False, ignore_index=True)
+
+    dataT.set_index(ts, inplace=True)
+    dataT.index.name = 'UTC'
+
+    return dataT
