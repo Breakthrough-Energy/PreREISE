6338ebf0fdb02c9ef8bcc5fff97e7af7a960d18b
Ben Rouillé d'Orfeuil <brdo@intven.com>
2018-10-11 09:51:15 -0700

    feat: Create function retrieve_data

--- prereise/gather/solardata/nsrdb/nsrdb.py
+++ prereise/gather/solardata/nsrdb/nsrdb.py
@@ -1,119 +1,85 @@
-import numpy as np
-import pandas as pd
 from collections import OrderedDict
 
-"""
-Collect a  set of solar and meteoroligical data fields from the National
-Solar Radiation Database (NSRDB).
-Website: https://nsrdb.nrel.gov
-API: https://developer.nrel.gov/docs/solar/nsrdb/psm3_data_download/
-
-Calculate the solar power output for a given plant based on the naive
-assumption that it is equal to the maximum capacity of the plant (GenMWMax)
-at maximum irradiance (GHI) over the time period considered.
-"""
-
-###############################################
-# Get the plants coordinates, id and GenMWMax #
-###############################################
-import westernintnet
-grid = westernintnet.WesternIntNet()
-
-solar_plant = grid.genbus.groupby('type').get_group('solar')
-print("There are %d solar plants in the Western grid." % len(solar_plant))
-
-coord = OrderedDict()
-for i in range(len(solar_plant)):
-    key = (str(solar_plant.lon.values[i]),str(solar_plant.lat.values[i]))
-    if key not in coord.keys():
-        coord[key] = [(solar_plant.index[i],solar_plant.GenMWMax.values[i])]
-    else:
-        coord[key].append((solar_plant.index[i],solar_plant.GenMWMax.values[i]))
-
-print("There are %d unique locations." % len(coord))
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
 
-#############################
-# Get Attributes from NSRDB #
-#############################
+def retrieve_data(solar_plant, year='2016'):
+    """Retrieve irradiance data from NSRDB.
 
-# api key
-api_key = '0neC1BX0VMIGFJWIXYA7Y9ZCjbmHeceLHjWiPjdf'
+    :param wind_farm: pandas DataFrame of wind farms.
+    :param year: year.
+    :return: pandas DataFrame with the columns: plant ID, timestamp ID and
+    power output (MW).
+    """
 
-# year
-year = 2016
+    # Information on solar plants
+    n_target = len(solar_plant)
 
-# Set the attributes to extract
-attributes = 'ghi'
+    # Identify unique location
+    coord = OrderedDict()
+    for i in range(n_target):
+        key = (str(solar_plant.lon.values[i]),
+               str(solar_plant.lat.values[i]))
+        if key not in coord.keys():
+            coord[key] = [(solar_plant.index[i],
+                           solar_plant.GenMWMax.values[i])]
+        else:
+            coord[key].append((solar_plant.index[i],
+                               solar_plant.GenMWMax.values[i]))
 
-# Set leap year to true or false. True will return leap day data if present,
-# false will not.
-leap_year = 'true' # 2016 is a leap year
+    # Build query
+    api_key = '0neC1BX0VMIGFJWIXYA7Y9ZCjbmHeceLHjWiPjdf'
+    attributes = 'ghi'
+    leap_day = 'true'
+    interval = '60'
+    utc = 'true'
+    name = 'Benjamin+Rouille+d+Orfeuil'
+    reason = 'Renewable+Energy+Studies'
+    affiliation = 'Intellectual+Ventures'
+    email = 'brdo@intven.com'
+    list = 'false'
 
-# Set time interval in minutes, i.e., '30' is half hour intervals.
-# Valid intervals are 30 & 60.
-interval = '60'
+    # URL
+    url = 'http://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?'
+    url = url + 'api_key={key}'.format(key=api_key)
 
-# Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will
-# use the local time zone of the data. NOTE: In order to use the NSRDB data in
-# SAM, you must specify UTC as 'false'. SAM requires the data to be in the
-# local time zone.
-utc = 'true'
+    payload = 'names={year}'.format(year=year) + '&' + \
+        'leap_day={leap}'.format(leap=leap_day) + '&' + \
+        'interval={interval}'.format(interval=interval) + '&' + \
+        'utc={utc}'.format(utc=utc) + '&' + \
+        'full_name={name}'.format(name=name) + '&' + \
+        'email={email}'.format(email=email) + '&' + \
+        'affiliation={affiliation}'.format(affiliation=affiliation) + '&' + \
+        'mailing_list={mailing_list}'.format(mailing_list=list) + '&' + \
+        'reason={reason}'.format(reason=reason) + '&' + \
+        'attributes={attr}'.format(attr=attributes)
 
-# Full name
-name = 'Benjamin+Rouille+d+Orfeuil'
+    data = pd.DataFrame({'Pout': [], 'plantID': [], 'ts': [], 'tsID': []})
 
-# Reason for using the NSRDB.
-reason = 'Renewable+Energy+Studies'
+    for key in tqdm(coord.keys(), total=len(coord)):
+        query = 'wkt=POINT({lon}%20{lat})'.format(lon=key[0], lat=key[1])
+        data_loc = pd.read_csv(url + '&' + payload + '&' + query, skiprows=2)
+        ghi = data_loc.GHI.values
+        data_loc = pd.DataFrame({'Pout': ghi})
+        data_loc['Pout'] /= max(ghi)
+        data_loc['tsID'] = range(1, len(ghi)+1)
+        data_loc['ts'] =  pd.date_range(start=year,
+                                        end=str(int(year)+1),
+                                        freq='H')[:-1]
 
-# Affiliation
-affiliation = 'Intellectual+Ventures'
+        for i in coord[key]:
+            data_site = data_loc.copy()
+            data_site['Pout'] *= i[1]
+            data_site['plantID'] = i[0]
 
-# Email address
-email = 'brdo@intven.com'
+            data = data.append(data_site, ignore_index=True, sort=False)
 
-# Mailing list subscription
-list = 'false'
+    data['plantID'] = data['plantID'].astype(np.int32)
+    data['tsID'] = data['tsID'].astype(np.int32)
 
-# URL
-url = 'http://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?'
-url = url + 'api_key={key}'.format(key=api_key)
+    data.sort_values(by=['tsID', 'plantID'], inplace=True)
+    data.reset_index(inplace=True, drop=True)
 
-payload = 'names={year}'.format(year=year) + '&' + \
-          'leap_day={leap}'.format(leap=leap_year) + '&' + \
-          'interval={interval}'.format(interval=interval) + '&' + \
-          'utc={utc}'.format(utc=utc) + '&' + \
-          'full_name={name}'.format(name=name) + '&' + \
-          'email={email}'.format(email=email) + '&' + \
-          'affiliation={affiliation}'.format(affiliation=affiliation) + '&' + \
-          'mailing_list={mailing_list}'.format(mailing_list=list) + '&' + \
-          'reason={reason}'.format(reason=reason) + '&' + \
-          'attributes={attr}'.format(attr=attributes)
-
-data = pd.DataFrame({'Pout':[], 'plantID':[], 'tsID':[]})
-
-for i, key in enumerate(coord.keys()):
-    print("Loading information for location #%d" % (i+1))
-    query = 'wkt=POINT({lon}%20{lat})'.format(lon=key[0], lat=key[1])
-    data_loc = pd.read_csv(url + '&' + payload + '&' + query, skiprows=2)
-    ghi = data_loc.GHI.values
-    data_loc = pd.DataFrame({'Pout':ghi})
-    data_loc['Pout'] /= max(ghi)
-    data_loc['tsID'] = range(1,len(ghi)+1)
-
-    for i in coord[key]:
-        data_site = data_loc.copy()
-        data_site['Pout'] *= i[1]
-        data_site['plantID'] = i[0]
-
-        data = data.append(data_site, ignore_index = True, sort = False)
-
-data['plantID'] = data['plantID'].astype(np.int32)
-data['tsID'] = data['tsID'].astype(np.int32)
-
-data.sort_values(by=['tsID', 'plantID'], inplace=True)
-data.reset_index(inplace=True, drop=True)
-
-# Write File
-name = "western_solar_output_%d_ts.txt" % (year)
-data.to_csv(name, header=None, index=False, columns=['tsID','plantID','Pout'])
+    return data
