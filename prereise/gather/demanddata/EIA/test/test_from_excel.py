2d8c0c74c7816f5e6547559e5eeaaa9ceda09355
kasparm <10716532+kasparm@users.noreply.github.com>
2018-10-22 08:51:23 -0700

    refactor: Changes in naming and data folder location

--- prereise/gather/demanddata/EIA/test/test_from_excel.py
+++ prereise/gather/demanddata/EIA/test/test_from_excel.py
@@ -1,13 +1,14 @@
-import pytest
-
 import json
-import numpy as np
-import pandas as pd
-from urllib.error import URLError, HTTPError
+import os
+from datetime import datetime, timedelta
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
-from datetime import datetime, timedelta
-from .. import getEIAdata
+import numpy as np
+import pandas as pd
+import pytest
+
+from .. import get_eia_data
 
 
 def test_from_excel():
@@ -18,12 +19,11 @@ def test_from_excel():
 
     '''
 
-    dir1 = './'
+    dir1 = os.path.join(os.path.dirname(__file__), 'data')
 
     start = pd.to_datetime('2018-07-01 07:00:00')
     end = pd.to_datetime('2018-10-01 07:00:00')
-    BA_list = ['BPAT','CISO','EPE']
+    ba_list = ['BPAT', 'CISO', 'EPE']
 
-    BA_from_excel = getEIAdata.from_excel(dir1, BA_list, start, end)
-    assert (len(BA_from_excel.columns) == len(BA_list))
-
+    ba_from_excel = get_eia_data.from_excel(dir1, ba_list, start, end)
+    assert (len(ba_from_excel.columns) == len(ba_list))
