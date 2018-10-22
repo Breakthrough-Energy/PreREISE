3772254f124abb99c783ff68ebe9aa3511dfb86c
kasparm <10716532+kasparm@users.noreply.github.com>
2018-10-22 08:43:31 -0700

    fix: fixed import in test_call and set test to skip

--- prereise/call/test/test_call.py
+++ prereise/call/test/test_call.py
@@ -1,8 +1,12 @@
+import os
+import sys
 import unittest
 
-import sys
-sys.path.append("..")
-import call
+import pytest
 
-call.launch_scenario_performance('texas_scenario')
+from ..call import launch_scenario_performance
 
+
+@pytest.mark.skip(reason="Need to run on the server")
+def test():
+    launch_scenario_performance('western_scenarioUnitTest02')
