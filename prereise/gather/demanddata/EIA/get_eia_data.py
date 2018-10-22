38464b89d9de1ee36f3b67dae687f4690b155730
kasparm <10716532+kasparm@users.noreply.github.com>
2018-10-22 08:50:14 -0700

    refactor: Changed nameing to follow code convention and simplifyed code

--- prereise/gather/demanddata/EIA/get_eia_data.py
+++ prereise/gather/demanddata/EIA/get_eia_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 from datetime import datetime
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
@@ -6,72 +7,64 @@ from urllib.request import urlopen
 import numpy as np
 import pandas as pd
 from dateutil.parser import parse
-from pandas.tseries.offsets import DateOffset 
+from pandas.tseries.offsets import DateOffset
 
 
-def from_download(tok, startdate, enddate, offsetdays, serieslist):
+def from_download(tok, start_date, end_date, offset_days, series_list):
     """Downloads and assemble dataset of demand data per balancing authority \ 
         for desired date range.
 
     :param string tok: token obtained by registering with EIA.
     :param timestamp start: start date.
     :param timestamp end: end data.
-    :param list serieslist: list of demand series names provided by EIA, \ 
+    :param list series_list: list of demand series names provided by EIA, \ 
         e.g., ['EBA.AVA-ALL.D.H', 'EBA.AZPS-ALL.D.H'].
-    :param int offsetdays: number of business days for data to stabilize.
+    :param int offset_days: number of business days for data to stabilize.
     :return: data frame indexed with hourly UTC time and BA series \ 
         name for column names.
     """
-    df = {}
 
-    for x in serieslist:
-        BA = x
-        print('Downloading', BA)
-        d = EIAgov(tok, [x])
-        df[BA] = d.GetData()
-        df[BA].index = pd.to_datetime(df[BA]['Date'])
-        df[BA].drop(columns=['Date'], inplace=True)
-
-    timespan = pd.date_range(startdate,
-                             enddate - DateOffset(days=offsetdays),
+    timespan = pd.date_range(start_date,
+                             end_date - DateOffset(days=offset_days),
                              freq='H')
-
     df_all = pd.DataFrame(index=timespan)
-    for x in serieslist:
-        df_all = pd.concat([df_all, df[x]], axis=1)
+
+    for ba in series_list:
+        print('Downloading', ba)
+        d = EIAgov(tok, [ba])
+        df = d.get_data()
+        df.index = pd.to_datetime(df['Date'])
+        df.drop(columns=['Date'], inplace=True)
+        df_all = pd.concat([df_all, df], axis=1)
 
     return df_all
 
 
-def from_excel(directory, serieslist, startdate, enddate):
+def from_excel(directory, series_list, start_date, end_date):
     """Assembles EIA balancing authority (BA) data from pre-downloaded Excel \ 
         spreadsheets. The spreadsheets contain data from July 2015 to present.
 
     :param string directory: location of Excel files.
-    :param list serieslist: list of BA initials, e.g., ['PSE',BPAT','CISO'].
-    :param timestamp startdate: desired start of dataset.
-    :param timestamp enddate: desired end of dataset.
+    :param list series_list: list of BA initials, e.g., ['PSE',BPAT','CISO'].
+    :param timestamp start_date: desired start of dataset.
+    :param timestamp end_date: desired end of dataset.
     :return: data frame indexed with hourly UTC time and BA series \ 
         name for column names.
     """
-
-    df = {}
-
-    for x in serieslist:
-        print(x)
-        filename = x + '.xlsx'
-        df[x] = pd.read_excel(io=directory + "/" + filename,
-                              header=0, usecols='B,U')
-        df[x].index = pd.to_datetime(df[x]['UTC Time'])
-        df[x] = df[x].resample('H').asfreq()  # Fill missing times
-        df[x].drop(columns=['UTC Time'], inplace=True)
-        df[x].rename(columns={"Published D": x}, inplace=True)
-
-    timespan = pd.date_range(startdate, enddate, freq='H')
-
+    timespan = pd.date_range(start_date, end_date, freq='H')
     df_all = pd.DataFrame(index=timespan)
-    for x in serieslist:
-        df_all = pd.concat([df_all, df[x]], join='inner', axis=1)
+
+    for ba in series_list:
+        print(ba)
+        filename = ba + '.xlsx'
+        df = pd.read_excel(io=os.path.join(directory, filename),
+                           header=0, usecols='B,U')
+        df.index = pd.to_datetime(df['UTC Time'])
+        # Fill missing times
+        df = df.resample('H').asfreq()
+        df.drop(columns=['UTC Time'], inplace=True)
+        df.rename(columns={"Published D": ba}, inplace=True)
+        df_all = pd.concat([df_all, df], join='inner', axis=1)
 
     return df_all
 
@@ -84,11 +77,11 @@ class EIAgov(object):
         :param string token: EIA token
         :param list series: id code(s) of the series to be downloaded
         """
-        
+
         self.token = token
         self.series = series
 
-    def Raw(self, ser):
+    def raw(self, ser):
         """Downloads json files from EIA.
 
         :param list ser: list of filenames.
@@ -113,11 +106,11 @@ class EIAgov(object):
             print('URL type error.')
             print('Reason: ', e.reason)
 
-    def GetData(self):
+    def get_data(self):
         """Convert json files into pandas DataFrame
         """
-        
-        date_ = self.Raw(self.series[0])
+
+        date_ = self.raw(self.series[0])
         date_series = date_['series'][0]['data']
         endi = len(date_series)
         date = []
@@ -129,7 +122,7 @@ class EIAgov(object):
 
         lenj = len(self.series)
         for j in range(lenj):
-            data_ = self.Raw(self.series[j])
+            data_ = self.raw(self.series[j])
             data_series = data_['series'][0]['data']
             data = []
             endk = len(date_series)
