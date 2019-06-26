eeccfb662f82c0ecc633c5d8d50cf45ad2d5d1a1
Ben RdO <brdo@intven.com>
2019-06-26 11:33:24 -0700

    docs: Update docstring

--- prereise/gather/hydrodata/eia/eia.py
+++ prereise/gather/hydrodata/eia/eia.py
@@ -4,14 +4,13 @@ from westernintnet.westernintnet import win_data
 
 
 def get_profile(start='2016-01-01-00', end='2016-12-31-23'):
-    """Creates hydro profile using monthly capacity factors reported by EIA \ 
-        in this `table \
-        <https://www.eia.gov/electricity/annual/html/epa_04_08_b.html>`_.
+    """Creates hydro profile from monthly capacity factors reported by EIA
+        `here <https://www.eia.gov/electricity/annual/html/epa_04_08_b.html>`_.
 
     :param string start: starting date.
     :param string end: ending date.
-    :return: (*pandas*) -- data frame formated for REISE. The power output \ 
-        is in MWh.
+    :return: (*pandas.DataFrame*) -- data frame formatted for REISE. The power
+        output is in MWh.
     """
     start = pd.Timestamp(start)
     end = pd.Timestamp(end)
@@ -22,7 +21,7 @@ def get_profile(start='2016-01-01-00', end='2016-12-31-23'):
     scaler = scaler.reindex(pd.date_range(start=scaler.index[0],
                                           end=scaler.index[-1],
                                           freq='H'))
-                                          
+
     if scaler.index.contains(start) is False:
             print("Starting date must be within [2015-01-15, 2017-12-15]")
             raise Exception("Invalid start date")
@@ -31,11 +30,11 @@ def get_profile(start='2016-01-01-00', end='2016-12-31-23'):
             print("Ending date must be within [2015-01-15, 2017-12-15]")
             raise Exception("Invalid end date")
 
-    if  start >=  end:
+    if start >= end:
             print("Starting date must be greater than ending date")
             raise Exception("Invalid end date")
 
-    scaler.interpolate(method='time', inplace=True)    
+    scaler.interpolate(method='time', inplace=True)
     scaler = scaler[start:end]
 
     hydro_plant = win_data.genbus.groupby('type').get_group('hydro')
@@ -45,5 +44,5 @@ def get_profile(start='2016-01-01-00', end='2016-12-31-23'):
         data[i] = data.cf * hydro_plant.loc[i].GenMWMax
 
     data.drop('cf', inplace=True, axis=1)
-    
+
     return data
