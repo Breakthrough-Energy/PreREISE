b1ec641080a5d2828547d71f5fac392321b71c1f
BainanXia <bxia@intven.com>
2019-11-15 10:36:48 -0800

    refactor: pass scenario object into function
    -Move all demo notebooks into a single demo folder
    -Move all csv xlsx files into data folder
    -Update setup.py
    -Pass scenario object and filename into function get_net_demand_profile_western.py and get _monthly_net_generation.py
    -Add leap_year indicator in scale_hourly_shape_profile_by_monthly_total.py

--- prereise/gather/hydrodata/eia/get_monthly_net_generation.py
+++ prereise/gather/hydrodata/eia/get_monthly_net_generation.py
@@ -1,39 +1,50 @@
+import os
 import pandas as pd
-def get_monthly_net_generation(state):
+
+
+def get_monthly_net_generation(state,filename):
     """Get monthly total hydro net generation for the query state in 2016 from EIA923
     :param str state: the query state
+    :param str filename: name of the reference file
     :return: (*list*) EIA_net_generation -- monthly hydro net generation of the query state in a list  
         of length 12
     :raise Exception: if state is invalid.
     """
     
     state_name = {'WA':{'Washington'},
-          'OR':{'Oregon'},
-          'CA':{'Northern California','Bay Area','Central California','Southwest California','Southeast California'},
-          'NV':{'Nevada'},
-          'AZ':{'Arizona'},
-          'UT':{'Utah'},
-          'NM':{'New Mexico Western'},
-          'CO':{'Colorado'},
-          'WY':{'Wyoming'},
-          'ID':{'Idaho'},
-          'MT':{'Montana Western'},
+                  'OR':{'Oregon'},
+                  'CA':{'Northern California','Bay Area','Central California',
+                        'Southwest California','Southeast California'},
+                  'NV':{'Nevada'},
+                  'AZ':{'Arizona'},
+                  'UT':{'Utah'},
+                  'NM':{'New Mexico Western'},
+                  'CO':{'Colorado'},
+                  'WY':{'Wyoming'},
+                  'ID':{'Idaho'},
+                  'MT':{'Montana Western'},
           }
               
     if state not in state_name:
         print("%s is incorrect. Possible states are: %s" %
                   (state, list(state_name.keys())))
         raise Exception('Invalid state')
-     
-    filename = 'EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision.xlsx'
-    # filedir = './f923_2016/'
-    filedir = './'
-    plant_generation = pd.read_excel(io = filedir + filename, header = 0, usecols = 'A,D,G,I,P,CB:CM',skiprows = range(5))
+        
+    filedir = os.path.join(os.path.join(os.path.dirname(__file__), '..'),'data')
+    plant_generation = pd.read_excel(io = os.path.join(filedir,filename),
+                                     header = 0, usecols = 'A,D,G,I,P,CB:CM',skiprows = range(5))
+    
     # Filter by state and fuel type, HYC: conventional; hydro HPS: pump hydro
-    net_generation_by_plant = plant_generation[(plant_generation['Plant State'] == state) & ((plant_generation['AER\nFuel Type Code'] == 'HYC')|(plant_generation['AER\nFuel Type Code'] == 'HPS'))].copy()
+    net_generation_by_plant = plant_generation[(plant_generation['Plant State'] == state) &
+                                               ((plant_generation['AER\nFuel Type Code'] == 'HYC')|
+                                                (plant_generation['AER\nFuel Type Code'] == 'HPS'))].copy()
+    
     # Drop unnecessary columns, plant ID, etc..
     net_generation = net_generation_by_plant.drop(net_generation_by_plant.columns[[0,1,2,3,4]], axis=1)
     net_generation = net_generation.replace('.',0)
-    # Get monthly total net generation by summing up across plants with all positive values, note that negative ones are included in actual demand
+    
+    # Get monthly total net generation by summing up across plants with all positive values 
+    # Note that negative ones are included in actual demand
     EIA_net_generation = list(net_generation.apply(lambda x: x[x>0].sum()).values)
+    
     return EIA_net_generation
--- prereise/gather/hydrodata/eia/scale_hourly_shape_profile_by_monthly_total.py
+++ prereise/gather/hydrodata/eia/scale_hourly_shape_profile_by_monthly_total.py
@@ -1,18 +1,25 @@
-def scale_hourly_shape_profile_by_monthly_total(hourly_profile,monthly_total):
+def scale_hourly_shape_profile_by_monthly_total(hourly_profile,monthly_total,leap_year=True):
     """Scale hourly shape profile based on monthly total profile
-    :param list hourly_profile: list length 8784
-    :param list monthly_total: list length 12
+    :param list hourly_profile: list of hourly profile
+    :param list monthly_total: list of monthly total
+    :param bool leap_year: indicates the query year is a leap year or not
     :return: (*list*) -- scaled_hourly_profile  
     """
-    days_per_month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    if leap_year:
+        days_per_month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    else:
+        days_per_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+        
     hour_num = [day*24 for day in days_per_month]
     hour_month_mapping = []
     month_factor = [0]*12
     hour_count = 0
+    
     for i,hour in enumerate(hour_num):
         hour_month_mapping += [i]*hour
         month_sum = sum(hourly_profile[hour_count:hour_count+hour])
         hour_count += hour
         month_factor[i] = monthly_total[i]/month_sum
     scaled_hourly_profile = [d*month_factor[hour_month_mapping[i]] for i,d in enumerate(hourly_profile)] 
+    
     return scaled_hourly_profile
