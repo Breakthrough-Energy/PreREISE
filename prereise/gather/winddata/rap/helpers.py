48e9087c8aa3469df0eeed9a32e5cee399a97598
Daniel Olsen <dolsen@intven.com>
2019-11-22 16:11:48 -0800

    feat: create state averaged wind power curves

--- prereise/gather/winddata/rap/helpers.py
+++ prereise/gather/winddata/rap/helpers.py
@@ -4,9 +4,51 @@ from os import path
 import numpy as np
 import pandas as pd
 
-filename = 'PowerCurves.csv'
-filepath = path.abspath(path.join(path.dirname(__file__), '..', filename))
-PowerCurves = pd.read_csv(filepath, index_col=0, header=None).T
+
+def build_state_power_curves(filepath):
+    """Parse Form 860 and turbine curves to obtain average state curves.
+
+    :param str filepath: path for output file.
+    :return: (*None*).
+    """
+    print('building state_power_curves')
+    mfg_col = 'Predominant Turbine Manufacturer'
+    model_col = 'Predominant Turbine Model Number'
+    hubheight_col = 'Turbine Hub Height (Feet)'
+    wspd_height_base = 262.467       #80m in feet
+    wspd_exp = 0.15
+    form_860_path = path.abspath(
+        path.join(path.dirname(__file__), '..', '3_2_Wind_Y2016.csv'))
+
+    form_860 = pd.read_csv(form_860_path, skiprows=1)
+    states = form_860['State'].unique()
+    base_x = PowerCurves['Speed bin (m/s)'].to_numpy()
+    curve_x = np.arange(base_x[0], base_x[-1], 0.01)
+    state_curves = pd.DataFrame(curve_x, columns=['Speed bin (m/s)'])
+    for s in states:
+        cumulative_curve = np.zeros_like(curve_x)
+        cumulative_capacity = 0
+        state_wind_farms = form_860[form_860['State'] == s]
+        for i, f in enumerate(state_wind_farms.index):
+            farm_capacity = state_wind_farms['Nameplate Capacity (MW)'].iloc[i]
+            hub_height = state_wind_farms[hubheight_col].iloc[i]
+            wspd_scale_factor = (wspd_height_base / hub_height)**wspd_exp
+            shifted_x = base_x * wspd_scale_factor
+            turbine_mfg = state_wind_farms[mfg_col].iloc[i]
+            turbine_model = state_wind_farms[model_col].iloc[i]
+            turbine_name = ' '.join([turbine_mfg, turbine_model])
+            if turbine_name in PowerCurves.columns:
+                turbine_curve = PowerCurves[turbine_name]
+            else:
+                turbine_curve = PowerCurves['IEC class 2']
+            shifted_curve = np.interp(
+                curve_x, shifted_x, turbine_curve, left=0, right=0)
+            cumulative_curve += shifted_curve * farm_capacity
+            cumulative_capacity += farm_capacity
+        average_curve = cumulative_curve / cumulative_capacity
+        state_curves[s] = average_curve
+    state_curves.set_index('Speed bin (m/s)', inplace=True)
+    state_curves.to_csv(filepath)
 
 
 def ll2uv(lon, lat):
@@ -51,14 +93,10 @@ def get_power(wspd, turbine):
     :param str turbine: class of turbine.
     :return: (*float*) -- normalized power.
     """
-    # "* 2" gives us ceil and floor with steps of 0.5
-    match = (PowerCurves['Speed bin (m/s)'] * 2 <= np.ceil(wspd * 2)) & \
-            (PowerCurves['Speed bin (m/s)'] * 2 >= np.floor(wspd * 2))
-    if not any(match):
-        return 0
     return np.interp(wspd,
-                     PowerCurves[turbine][match].index.values,
-                     PowerCurves[turbine][match].values)
+                     PowerCurves[turbine].index.values,
+                     PowerCurves[turbine].values,
+                     left=0, right=0)
 
 
 def to_reise(data):
@@ -84,3 +122,14 @@ def to_reise(data):
     profile.index.name = 'UTC'
 
     return profile
+
+
+filename = 'PowerCurves.csv'
+filepath = path.abspath(path.join(path.dirname(__file__), '..', filename))
+PowerCurves = pd.read_csv(filepath, index_col=0, header=None).T
+
+filename = 'StatePowerCurves.csv'
+filepath = path.abspath(path.join(path.dirname(__file__), '..', filename))
+if not path.isfile(filepath):
+    build_state_power_curves(filepath)
+StatePowerCurves = pd.read_csv(filepath)
