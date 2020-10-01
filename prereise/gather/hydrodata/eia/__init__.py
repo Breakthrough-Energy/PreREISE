8d2f24614568ecc480d58d41a963578f49ddf013
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2020-09-30 22:37:54 -0700

    chore, refactor: rename function/module, update calculations in hydrodata

--- prereise/gather/hydrodata/eia/__init__.py
+++ prereise/gather/hydrodata/eia/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
-    "get_profile_by_us_monthly_factor",
-    "get_hourly_plant_level_hydro_profile",
-    "get_net_demand_profile_western",
-    "scale_hourly_shape_profile_by_monthly_total",
+    "decompose_profile",
+    "helpers",
+    "interpolate_capacity_factors",
+    "net_demand",
 ]
