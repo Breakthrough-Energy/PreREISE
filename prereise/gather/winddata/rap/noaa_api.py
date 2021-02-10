500dedff186daabb439c64b3e0d1083263896604
jon-hagg <66005238+jon-hagg@users.noreply.github.com>
2021-02-09 22:08:38 -0800

    fix: use fallback logic (#135)

--- prereise/gather/winddata/rap/noaa_api.py
+++ prereise/gather/winddata/rap/noaa_api.py
@@ -11,8 +11,8 @@ class NoaaApi:
     :raises ValueError: if box is missing keys or contains unknown keys
     """
 
-    base_url = "https://www.ncdc.noaa.gov/thredds/ncss/model-rap130/"
-    fallback_url = "https://www.ncdc.noaa.gov/thredds/ncss/model-rap130-old/"
+    base_url = "https://www.ncei.noaa.gov/thredds/ncss/model-rap130/"
+    fallback_url = "https://www.ncei.noaa.gov/thredds/ncss/model-rap130-old/"
     var_u = "u-component_of_wind_height_above_ground"
     var_v = "v-component_of_wind_height_above_ground"
 
@@ -93,12 +93,12 @@ class NoaaApi:
         for time_slice in self.iter_hours(start, end):
             response = download(time_slice)
             if response.status_code == 404:
-                print(f"Got 404 response, trying fallback url. Original={url}")
-                download(time_slice, fallback=True)
+                print(f"Got 404 response, trying fallback url.")
+                response = download(time_slice, fallback=True)
                 if response.status_code == 404:
                     print(
                         "Content not found for the given range - it may be"
-                        + "available via tape archive, please contact NOAA for"
-                        + "support"
+                        + " available via tape archive, please contact NOAA for"
+                        + " support"
                     )
             yield response
