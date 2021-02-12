01ab0a9bd8061b81f55fa5fa562218b26a54e015
Kaspar Mueller <kaspar.mueller@breakthroughenergy.org>
2021-02-12 15:12:19 -0800

    chore: update package version number

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="prereise",
-    version="0.3",
+    version="0.4",
     description="Create and run an energy scenario",
     url="https://github.com/Breakthrough-Energy/PreREISE",
     author="Kaspar Mueller",
-    author_email="kaspar.mueller@breakthroughenergy.org",
+    author_email="kaspar@breakthroughenergy.org",
     packages=find_packages(),
     package_data={
         "prereise": [
