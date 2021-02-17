b7532ad6f6a50fc89b92762faa22fe7628360878
danielolsen <danielolsen@users.noreply.github.com>
2021-02-17 10:40:37 -0800

    refactor: upgrade pyproj (#147)
    * test: add test for ll2ij
    
    * refactor: change pyproj calls to 2.x syntax
    
    * chore: update pyproj requirement to 2.6.1
    
    * refactor: re-use Transformer object for faster transforms

--- prereise/gather/solardata/ga_wind/helpers.py
+++ prereise/gather/solardata/ga_wind/helpers.py
@@ -1,10 +1,17 @@
 import numpy as np
-from pyproj import Proj
+
+proj_string = (
+    "+proj=lcc +lat_1=30 +lat_2=60"
+    " +lat_0=38.47240422490422 +lon_0=-96.0"
+    " +x_0=0 +y_0=0 +ellps=sphere"
+    " +units=m +no_defs"
+)
 
 
-def ll2ij(lon_origin, lat_origin, lon, lat):
+def ll2ij(transformer, lon_origin, lat_origin, lon, lat):
     """Finds nearest x/y indices for a given lat/lon.
 
+    :param pyproj.transformer.Transformer: transformer object used for reprojection.
     :param float lat_origin: latitude of coordinate of origin.
     :param float lon_origin: longitude of coordinate of origin.
     :param float lat: latitude of coordinate of interest.
@@ -12,15 +19,8 @@ def ll2ij(lon_origin, lat_origin, lon, lat):
     :return: (*tuple*) -- coordinate of the closest pixel in the database.
     """
 
-    proj_string = """+proj=lcc +lat_1=30 +lat_2=60
-                    +lat_0=38.47240422490422 +lon_0=-96.0
-                    +x_0=0 +y_0=0 +ellps=sphere
-                    +units=m +no_defs"""
-
-    proj2grid = Proj(proj_string)
-
-    origin_xy = proj2grid(lon_origin, lat_origin)
-    target_xy = proj2grid(lon, lat)
+    origin_xy = transformer.transform(lon_origin, lat_origin)
+    target_xy = transformer.transform(lon, lat)
 
     delta = np.subtract(target_xy, origin_xy)
 
