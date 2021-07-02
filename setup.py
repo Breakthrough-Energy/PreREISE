c3802263378ceb68fb789989c7ddb69b78fe563e
BainanXia <bainan.xia@breakthroughenergy.org>
2021-07-02 14:15:19 -0700

    fix: get all data in the data folder installed with the package

--- setup.py
+++ setup.py
@@ -10,10 +10,8 @@ setup(
     packages=find_packages(),
     package_data={
         "prereise": [
-            "gather/winddata/data/*.csv",
-            "gather/data/EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision.xlsx",
-            "gather/hydrodata/data/*.csv",
-            "gather/solardata/data/*.csv",
+            "gather/*/data/*",
+            "gather/data/*",
         ]
     },
     zip_safe=False,
