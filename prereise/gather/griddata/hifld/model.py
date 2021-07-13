3e10bf5c6245b88a5a0dac935f4ea81375e07ac2
ahurli <anna.hurlimann@breakthroughenergy.org>
2021-07-13 11:40:01 -0700

    feat: load initial csvs

---
+++ prereise/gather/griddata/hifld/model.py
@@ -0,0 +1,17 @@
+from prereise.gather.griddata.hifld.data_access.load import load_csv
+
+def get_grid_data(e_csv, t_csv, z_csv):
+    """Load HIFLD data and create dataframes corresponding to the input csv's for the BES grid.
+
+    :param str e_csv: path of the HIFLD substation csv file
+    :param str t_csv: path of the HIFLD transmission csv file
+    :param str z_csv: path of the zone csv file
+    """
+    
+    zone_data = load_csv(z_csv)
+
+    sub_data = load_csv(e_csv, dtypes={"COUNTYFIPS": str})
+
+    line_data = load_csv(t_csv)
+
+    return zone_data, sub_data, line_data
