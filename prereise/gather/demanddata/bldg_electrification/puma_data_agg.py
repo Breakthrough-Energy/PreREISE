e568a5aabd9e0c1c7d3b4eab9407fe94b234649c
Daniel Olsen <daniel@breakthroughenergy.org>
2021-08-23 13:14:38 -0700

    style: clean up style within aggregate_puma_df

--- prereise/gather/demanddata/bldg_electrification/puma_data_agg.py
+++ prereise/gather/demanddata/bldg_electrification/puma_data_agg.py
@@ -17,7 +17,7 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
     """
     # Set up puma_df data frame
     puma_df = puma_fuel_2010["state"].to_frame()
-    
+
     # Initialize columns that will be created via summing/averaging
     sum_columns = [
         "pop_2010",
@@ -28,19 +28,29 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
         "hdd65_normals_2010",
         "cdd65_normals_2010",
     ]
-    
+
     # Load RECS and CBECS area scales for res and com
     resscales = pd.read_csv(os.path.join(data_dir, "area_scale_res.csv"))
     comscales = pd.read_csv(os.path.join(data_dir, "area_scale_com.csv"))
-    
+
     # Interpolate a 2010 area to scale model area to corresponding RECS/CBECS area
     resscales["2010_scalar"] = (
-        resscales["RECS2009"] + (resscales["RECS2015"] - resscales["RECS2009"]) * ((const.target_year-const.recs_date_1) / (const.recs_date_2-const.recs_date_1))
+        resscales["RECS2009"]
+        + (resscales["RECS2015"] - resscales["RECS2009"])
+        * (
+            (const.target_year - const.recs_date_1)
+            / (const.recs_date_2 - const.recs_date_1)
+        )
     ) / resscales["Model2010"]
     comscales["2010_scalar"] = (
-        comscales["CBECS2003"] + (comscales["CBECS2012"] - comscales["CBECS2003"]) * ((const.target_year-const.cbecs_date_1) / (const.cbecs_date_2-const.cbecs_date_1))
+        comscales["CBECS2003"]
+        + (comscales["CBECS2012"] - comscales["CBECS2003"])
+        * (
+            (const.target_year - const.cbecs_date_1)
+            / (const.cbecs_date_2 - const.cbecs_date_1)
+        )
     ) / comscales["Model2010"]
-    
+
     # Collect all state data into one data frame
     tract_data = pd.concat(
         [
@@ -48,18 +58,20 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
             for state in const.state_list
         ]
     )
-    
+
     # Rename tract IDs to match all dataframe naming conventions
     tract_data["id"] = ["tract_" + str(i).rjust(11, "0") for i in tract_data["id"]]
     tract_data["id"] = tract_data["id"].astype("str")
     tract_data.set_index("id", inplace=True)
-    
+
     # Sum population, housing units, and areas
     grouped_tracts = tract_data.groupby(tract_puma_mapping["puma"])
     for col in sum_columns:
         col_to_sum = col.replace("_gbs", "").replace("_", ".")
-        puma_df.loc[grouped_tracts.groups.keys(), col] = grouped_tracts[col_to_sum].sum()
-    
+        puma_df.loc[grouped_tracts.groups.keys(), col] = grouped_tracts[
+            col_to_sum
+        ].sum()
+
     # Population-weighted average hdd, cdd, and acpen
     for col in weighted_sum_columns:
         col_to_sum = col.replace("_normals_2010", "").replace("_res_2010", ".res")
@@ -68,7 +80,7 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
             weighted_elements.groupby(tract_puma_mapping["puma"]).sum()
             / tract_data["pop.2010"].groupby(tract_puma_mapping["puma"]).sum()
         )
-    
+
     # Scale puma area from gbs to 2010 RECS/CBECS
     for state in const.state_list:
         state_row_scale_res = resscales[resscales.eq(state).any(1)].reset_index()
@@ -81,7 +93,7 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
         puma_df.loc[puma_df["state"] == state, "com_area_2010_m2"] = (
             puma_df[puma_df["state"] == state]["com_area_gbs_m2"] * com_scalar
         )
-    
+
     # Calculate res fractions of fuel usage based off puma_fuel_2010 household data
     puma_df["frac_sh_res_natgas"] = (
         puma_fuel_2010["hh_utilgas"] / puma_fuel_2010["hh_total"]
@@ -92,9 +104,13 @@ def aggregate_puma_df(puma_fuel_2010, tract_puma_mapping):
     )
     puma_df["frac_sh_res_coal"] = puma_fuel_2010["hh_coal"] / puma_fuel_2010["hh_total"]
     puma_df["frac_sh_res_wood"] = puma_fuel_2010["hh_wood"] / puma_fuel_2010["hh_total"]
-    puma_df["frac_sh_res_solar"] = puma_fuel_2010["hh_solar"] / puma_fuel_2010["hh_total"]
+    puma_df["frac_sh_res_solar"] = (
+        puma_fuel_2010["hh_solar"] / puma_fuel_2010["hh_total"]
+    )
     puma_df["frac_sh_res_elec"] = puma_fuel_2010["hh_elec"] / puma_fuel_2010["hh_total"]
-    puma_df["frac_sh_res_other"] = puma_fuel_2010["hh_other"] / puma_fuel_2010["hh_total"]
+    puma_df["frac_sh_res_other"] = (
+        puma_fuel_2010["hh_other"] / puma_fuel_2010["hh_total"]
+    )
     puma_df["frac_sh_res_none"] = puma_fuel_2010["hh_none"] / puma_fuel_2010["hh_total"]
 
     return puma_df
