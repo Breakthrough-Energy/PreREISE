b9bf7d02ec9ff3c443d1984abcfbb2d068e3dcd7
BainanXia <bainan.xia@breakthroughenergy.org>
2021-09-03 12:48:04 -0500

    feat: add function to find adjacency states between two connected components

--- prereise/gather/griddata/hifld/data_process/topology.py
+++ prereise/gather/griddata/hifld/data_process/topology.py
@@ -1,4 +1,10 @@
-from itertools import product
+from itertools import combinations, product
+
+import networkx as nx
+from powersimdata.utility.distance import haversine
+from tqdm import tqdm
+
+from prereise.gather.griddata.hifld.const import abv_state_neighbor
 
 
 def min_dist_of_2_conn_comp(nodes1, nodes2, dist_metric):
@@ -22,3 +28,28 @@ def min_dist_of_2_conn_comp(nodes1, nodes2, dist_metric):
         if tmp_dist < min_dist:
             min_dist, res_v1, res_v2 = tmp_dist, v1, v2
     return min_dist, res_v1, res_v2
+
+
+def find_adj_state_of_2_conn_comp(cc1, cc2, state_adj, subs):
+    """Find the adjacency states between two given connected components.
+
+    :param int/iterable cc1: substation ids of the first connected component
+    :param int/iterable cc2: substation ids of the second connected component
+    :param dict state_adj: a dictionary defines state neighbors, keys are state
+        abbreviations, values are list of neighbor state abbreviations.
+    :param pandas.DataFrame subs: substation data frame indexed by substation ids,
+        contains a ``STATE`` column that defines the state of each substation.
+    :return: (*tuple*) -- a pair of sets represents states within each connected
+        component that are adjacent to the other one.
+    """
+    cc1_all_state = set(subs.loc[cc1].STATE.unique())
+    cc2_all_state = set(subs.loc[cc2].STATE.unique())
+    cc1_neighbors = cc1_all_state.copy()
+    cc2_neighbors = cc2_all_state.copy()
+    for s in cc1_all_state:
+        cc1_neighbors |= set(state_adj[s])
+    for s in cc2_all_state:
+        cc2_neighbors |= set(state_adj[s])
+    cc1_adj = cc2_neighbors & cc1_all_state
+    cc2_adj = cc1_neighbors & cc2_all_state
+    return cc1_adj, cc2_adj
