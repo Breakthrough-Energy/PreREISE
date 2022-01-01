322432d27f053e0c1ed44b94b0e3048421f71735
Lane Smith <lane@breakthroughenergy.org>
2021-12-31 20:20:42 -0800

    feat: add ability to specify interconnect in EFS profile creator

--- prereise/gather/demanddata/nrel_efs/map_states.py
+++ prereise/gather/demanddata/nrel_efs/map_states.py
@@ -2,10 +2,17 @@ from collections import defaultdict
 
 import pandas as pd
 from powersimdata.input.grid import Grid
-from powersimdata.network.usa_tamu.constants.zones import abv2state, id2abv, id2timezone
+from powersimdata.network.usa_tamu.constants.zones import (
+    abv2state,
+    id2abv,
+    id2timezone,
+    interconnect2id,
+)
 
 
-def decompose_demand_profile_by_state_to_loadzone(df, profile_type, save=None):
+def decompose_demand_profile_by_state_to_loadzone(
+    df, profile_type, regions=None, save=None
+):
     """Transforms the sectoral demand data so that it is separated by load zone rather
     than by state.
 
@@ -15,20 +22,36 @@ def decompose_demand_profile_by_state_to_loadzone(df, profile_type, save=None):
         that are output from :py:func:`partition_flexibility_by_sector`.
     :param str profile_type: A str that identifies the type of profile that is provided.
         Can be one of *'demand'* or *'demand_flexibility'*.
+    :param set/list regions: The combination of interconnection names and state
+        abbreviations that dictate the zone IDs to be included. Can choose any of:
+        *'Eastern'*, *'Western'*, *'Texas'*, any state abbreviation in the contiguous
+        United States, or *'All'*. Defaults to None.
     :param str save: Saves a .csv if a str representing a valid file path and file
         name is provided. Defaults to None, indicating that a .csv file should not be
         saved.
     :return: (*pandas.DataFrame*) -- Sectoral demand, split by load zone ID.
-    :raises TypeError: if df is not a pandas.DataFrame or if save is not input as a str.
+    :raises TypeError: if df is not a pandas.DataFrame, if profile_type is not a str,
+        if regions is not a set or list, if the components of regions are not str, or
+        if save is not input as a str.
     :raises ValueError: if df does not have the proper timestamps or the correct number
-        of states.
+        of states, if profile_type is not valid, or if the components of regions are
+        not valid.
     """
+
+    # Account for the immutable default parameter
+    if regions is None:
+        regions = {"All"}
 
     # Check the data types of the inputs
     if not isinstance(df, pd.DataFrame):
         raise TypeError("df must be input as a pandas.DataFrame.")
     if not isinstance(profile_type, str):
         raise TypeError("profile_type must be input as a str.")
+    if not isinstance(regions, (set, list)):
+        raise TypeError(
+            "Combination of interconnections and state abbreviations must be input as "
+            + "a set or list."
+        )
 
     # Check the demand DataFrame timestamps and column headers
     if not df.index.equals(
@@ -41,6 +64,24 @@ def decompose_demand_profile_by_state_to_loadzone(df, profile_type, save=None):
     # Check the value of profile_type
     if profile_type not in {"demand", "demand_flexibility"}:
         raise ValueError(f"{profile_type} is not a valid selection for profile_type.")
+
+    # Check that the components of regions are str
+    if not all(isinstance(x, str) for x in regions):
+        raise TypeError(
+            "Individual interconnections and state abbreviations must be input as a "
+            + "str."
+        )
+
+    # Reformat components of regions
+    regions = {x.upper() if len(x) == 2 else x.capitalize() for x in regions}
+    if "All" in regions:
+        regions = {"Eastern", "Western", "Texas"}
+
+    # Check that the components of regions are valid
+    possible_regions = ({"Eastern", "Western", "Texas"} | set(abv2state)) - {"AK", "HI"}
+    if not regions.issubset(possible_regions):
+        invalid_regions = regions - possible_regions
+        raise ValueError(f'Invalid regions: {", ".join(invalid_regions)}')
 
     # Grab the grid information
     grid = Grid(["USA"])
@@ -60,9 +101,26 @@ def decompose_demand_profile_by_state_to_loadzone(df, profile_type, save=None):
     # Convert from local hours to UTC time
     df_lz = shift_local_time_by_loadzone_to_utc(df_lz)
 
+    # Create mapping from state abbreviations to loadzone IDs
+    abv2id = {}
+    for k, v in id2abv.items():
+        abv2id.setdefault(v, []).append(k)
+
+    # Determine the loadzones to be inlcuded in the profile, as specified by regions
+    loadzones = set()
+    for region in regions:
+        if len(region) == 2:
+            loadzones = loadzones | set(abv2id[region])
+        else:
+            loadzones = loadzones | interconnect2id[region]
+    loadzones = list(sorted(loadzones))
+
+    # Keep the appropriate loadzones
+    df_lz = df_lz[loadzones]
+
     # Change the column headers if the profile_type is "demand_flexibility"
     if profile_type == "demand_flexibility":
-        df_lz.columns = ["zone." + str(x) for x in list(id2abv)]
+        df_lz.columns = ["zone." + str(x) for x in loadzones]
 
     # Save the demand data, if desired
     if save is not None:
