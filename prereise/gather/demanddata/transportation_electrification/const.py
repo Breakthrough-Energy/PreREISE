92ee7470996e3aec7956fbbac59c01d004c14117
Dianne Ison <isond@uci.edu>
2022-02-08 12:42:22 -0800

    add refactored immediate charging files

---
+++ prereise/gather/demanddata/transportation_electrification/const.py
@@ -0,0 +1,39 @@
+model_year = 2040
+
+nhts_census_column_names = [
+    "Household",
+    "Vehicle ID",
+    "Person ID",
+    "Scaling Factor Applied",
+    "Trip Number",
+    "Date",
+    "Day of Week",
+    "If Weekend",
+    "Trip start time (HHMM)",
+    "Trip end time (HHMM)",
+    "Travel Minutes",
+    "Dwell time",
+    "Miles traveled",
+    "Vehicle miles traveled",
+    "why from",
+    "why to",
+    "Vehicle type",
+    "Household vehicle count",
+    "Household size",
+    "Trip type",
+    "Start time (hour decimal)",
+    "End time (hour decimal)",
+    "Dwell time (hour decimal)",
+    "Travel time (hour decimal)",
+    "Vehicle speed (mi/hour)",
+    "sample vehicle number",
+    "total vehicle trips",
+    "total vehicle miles traveled",
+]
+
+# from grid to battery efficiency
+charging_efficiency = 0.9
+
+safety_coefficient = 1
+
+ER = 1
