dba55738fb27c57181ef505d8590d7c36b583806
Dianne Ison <isond@uci.edu>
2022-04-08 10:44:06 -0700

    refactor: add get_total_daily_vmt function

--- prereise/gather/demanddata/transportation_electrification/dwelling.py
+++ prereise/gather/demanddata/transportation_electrification/dwelling.py
@@ -11,8 +11,8 @@ def get_segment(dwelling_start_time, total_dwell_period):
 def get_location(location_strategy, dwell_location):
     """Determines if the vehicle can be charged given location strategy and dwelling location
 
-    :param int location_strategy: where the vehicle can charge-1, 2, 3, 4, 5, or 6; 
-        1-home only, 2-home and work related, 3-anywhere if possibile, 
+    :param int location_strategy: where the vehicle can charge-1, 2, 3, 4, 5, or 6;
+        1-home only, 2-home and work related, 3-anywhere if possibile,
         4-home and school only, 5-home and work and school, 6-only work
     :param int dwell_location: location the vehicle dwells
     :return: (*bool*) -- a boolean that represents whether or not the vehicle can charge
@@ -23,15 +23,15 @@ def get_location(location_strategy, dwell_location):
         3: True,
         4: dwell_location in {1, 21},
         5: dwell_location in {1, 11, 12, 21},
-        6: (dwell_location >= 10) or (dwell_location < 13)
+        6: (dwell_location >= 10) or (dwell_location < 13),
     }
     return location_allowed[location_strategy]
 
 
 def consider_trip_number(trip_strategy, total_trips, trip_num):
     """Determines if the vehicle should charge given trip strategy and current trip
-    
-    :param int trip_strategy: a toggle that determines if should charge on any trip or only 
+
+    :param int trip_strategy: a toggle that determines if should charge on any trip or only
         after last trip (1-anytrip number, 2-last trip)
     :param int total_trips: total trips that the vehicle makes
     :param int trip_num: the trip number of the current trip
@@ -39,7 +39,7 @@ def consider_trip_number(trip_strategy, total_trips, trip_num):
     """
     if trip_strategy == 1:
         return True
-    
+
     elif trip_strategy == 2:
         return total_trips == trip_num
 
@@ -50,15 +50,17 @@ def get_charging_power(power, trip_strategy, location_strategy, trip_data):
 
     :param int power: charger power, EVSE kW.
     :param int trip_strategy: a flag that determines which trips to consider, 1-anytrip number, 2-last trip.
-    :param int location_strategy: where the vehicle can charge-1, 2, 3, 4, 5, or 6; 
-        1-home only, 2-home and work related, 3-anywhere if possibile, 
+    :param int location_strategy: where the vehicle can charge-1, 2, 3, 4, 5, or 6;
+        1-home only, 2-home and work related, 3-anywhere if possibile,
         4-home and school only, 5-home and work and school, 6-only work
     :param pandas.Series trip_data: Row of DataFrame that has data for the trip we are calculating for.
     :return: (*float*) -- charging power.
     """
-    dwelling = trip_data['Dwell time (hour decimal)'] > 0.2
-    location = get_location(location_strategy, trip_data['why to'])
-    trip = consider_trip_number(trip_strategy, trip_data['total vehicle trips'], trip_data['trip number'])
+    dwelling = trip_data["Dwell time (hour decimal)"] > 0.2
+    location = get_location(location_strategy, trip_data["why to"])
+    trip = consider_trip_number(
+        trip_strategy, trip_data["total vehicle trips"], trip_data["trip number"]
+    )
     battery = 1
 
     if not all([dwelling, location, trip, battery]):
@@ -67,7 +69,9 @@ def get_charging_power(power, trip_strategy, location_strategy, trip_data):
     return power
 
 
-def get_energy_limit(power, segment, dwelling_start_time, dwelling_length, total_dwell_period):
+def get_energy_limit(
+    power, segment, dwelling_start_time, dwelling_length, total_dwell_period
+):
     """Determines the energy limit
 
     :param int power: charger power, EVSE kW.
@@ -85,10 +89,10 @@ def get_energy_limit(power, segment, dwelling_start_time, dwelling_length, total
 
     elif segment == 2:
         energy_limit = [power * t1, power * tn]
-    
+
     else:
         energy_limit = [power * t1]
-        energy_limit += [power for s in range(segment-1)]
+        energy_limit += [power for s in range(segment - 1)]
         energy_limit += [power * tn]
 
     return energy_limit
