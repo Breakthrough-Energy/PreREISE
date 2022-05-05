cf89d1d6a5045484bc8cfff9ea3229a5c7d54cfa
Dianne Ison <isond@uci.edu>
2022-05-05 03:17:04 -0700

    refactor: update get_energy_limit calculation

--- prereise/gather/demanddata/transportation_electrification/dwelling.py
+++ prereise/gather/demanddata/transportation_electrification/dwelling.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def get_segment(dwelling_start_time, dwelling_length):
     """Get dwelling activity segment.
@@ -12,7 +13,7 @@ def get_segment(dwelling_start_time, dwelling_length):
 
 
 def get_energy_limit(
-    power, segment, dwelling_start_time, dwelling_length, total_dwell_period
+    power, segment, dwelling_start_time, dwelling_length, charging_efficiency
 ):
     """Determines the energy limit for the entire dwelling period. Takes into
     consideration if charging does not start or end directly on the hour.
@@ -21,22 +22,25 @@ def get_energy_limit(
     :param int segment: the amount of the rates segments the dwelling activity possess.
     :param float dwelling_start_time: dwelling start time.
     :param float dwelling_length: dwelling end time.
-    :param float total_dwell_period: the total dwelling period.
+    :param float charging_efficiency: grid to battery efficiency.
     :return: (*list*) -- list of energy limits during the time span of available charging
     """
-    partial_start_time = round(dwelling_start_time) + 1 - dwelling_start_time
-    partial_end_time = dwelling_start_time + dwelling_length - round(total_dwell_period)
+    total_dwell_period = dwelling_start_time + dwelling_length
+    partial_start_time = math.floor(dwelling_start_time) + 1 - dwelling_start_time
+    partial_end_time = dwelling_start_time + dwelling_length - math.floor(total_dwell_period)
+
+    energy = power * charging_efficiency
 
     if segment == 1:
-        energy_limit = [power * dwelling_length]
+        energy_limit = [energy * dwelling_length]
 
     elif segment == 2:
-        energy_limit = [power * partial_start_time, power * partial_end_time]
-
+        energy_limit = [energy * partial_start_time, energy * partial_end_time]
+    
     else:
-        energy_limit = [power * partial_start_time]
-        energy_limit += [power for s in range(segment - 1)]
-        energy_limit += [power * partial_end_time]
+        energy_limit = [energy * partial_start_time]
+        energy_limit += [energy] * (int(segment)-2)
+        energy_limit += [energy * partial_end_time]
 
     return energy_limit
 
