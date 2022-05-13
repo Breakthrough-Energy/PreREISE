10ab96025dca3fdc2a06e25fc327bd2a03035670
danielolsen <danielolsen@users.noreply.github.com>
2022-05-13 16:04:51 -0700

    fix: improve interpretation of NOT AVAILABLE values when loading substation information (#294)

--- prereise/gather/griddata/hifld/data_access/load.py
+++ prereise/gather/griddata/hifld/data_access/load.py
@@ -182,7 +182,10 @@ def get_hifld_electric_substations(path):
         pd.read_csv(path)
         .drop(columns=["OBJECTID"])
         .round({"MAX_VOLT": 3, "MIN_VOLT": 3})
+        .replace(to_replace={"ZIP": "NOT AVAILABLE"}, value=None)
+        .query("COUNTRY == 'USA'")
     )
+    data["COUNTYFIPS"] = data["COUNTYFIPS"].astype(int)
 
     return data.query(
         "(STATUS == 'IN SERVICE' or STATUS == 'NOT AVAILABLE') and STATE in @abv2state"
