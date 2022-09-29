3c59a6d422af9d3d677892831f69008570930339
dmuldrew <dmuldrew@gmail.com>
2022-09-29 12:47:21 -0700

    refactor: move to common data columns (#311)

--- prereise/gather/demanddata/transportation_electrification/const.py
+++ prereise/gather/demanddata/transportation_electrification/const.py
@@ -1,5 +1,52 @@
 model_year = 2040
 
+ldv_location_allowed = {
+    1: {1},  # home only
+    2: set(range(1, 13)),  # home and go to work related
+    4: {1, 21},  # home and school only
+    5: {1, 11, 12, 21},  # home and work and school
+    6: {10, 11, 12},  # only work
+}
+
+hdv_location_allowed = {
+    1: {1},  # base only
+}
+
+why_to_list = [
+    1,
+    53,  # home related
+    10,
+    11,
+    12,  # work related
+    13,
+    14,
+    17,
+    20,
+    21,
+    22,
+    23,  # other
+    24,
+    30,
+    40,
+    41,
+    42,
+    50,
+    51,
+    52,
+    54,
+    55,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    80,
+    81,
+    82,
+    83,
+]
+
 nhts_census_column_names = [
     "Household",
     "Vehicle ID",
@@ -31,6 +78,17 @@ nhts_census_column_names = [
     "total vehicle miles traveled",
 ]
 
+ldv_columns_transform = {
+    "sample vehicle number": "vehicle_number",
+    "why to": "why_to",
+    "why from": "why_from",
+    "trip number": "trip_number",
+    "Miles traveled": "trip_miles",
+    "total vehicle trips": "total_trips",
+    "Dwell time (hour decimal)": "dwell_time",
+    "End time (hour decimal)": "trip_end",
+}
+
 hdv_data_column_names = [
     "Vehicle Number",
     "Trip Number",
@@ -47,9 +105,21 @@ hdv_data_column_names = [
     "Total Vehicle Miles",
 ]
 
+hdv_columns_transform = {
+    "Vehicle Number": "vehicle_number",
+    "Home base end (1/0)": "why_to",
+    "Trip Number": "trip_number",
+    "Trip Distance": "trip_miles",
+    "Total Vehicle Trips": "total_trips",
+    "Dwell Time": "dwell_time",
+    "Trip End": "trip_end",
+}
+
 # from grid to battery efficiency
 charging_efficiency = 0.9
 
 safety_coefficient = 1
 
 ER = 1
+
+emfacvmt = 758118400
