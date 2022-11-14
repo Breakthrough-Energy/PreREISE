f2b119864ea59248dbf5b093f380684f41097e38
Daniel Muldrew <dmuldrew@gmail.com>
2022-11-14 15:44:55 -0800

    test: update immediate scaling tests

--- prereise/gather/demanddata/transportation_electrification/tests/test_immediate.py
+++ prereise/gather/demanddata/transportation_electrification/tests/test_immediate.py
@@ -8,8 +8,8 @@ from prereise.gather.demanddata.transportation_electrification.immediate import 
 
 
 def test_apply_daily_adjustments():
-    hourly_profile = np.array([60, 180, 120, 120, 240, 60, 180, 220, 200, 20, 50, 30])
-    adjustment_values = pd.DataFrame([0.6, 0.7, 0.3, 0.5])
+    hourly_profile = np.array([80, 180, 140, 90, 180, 30, 280, 320, 200, 20, 50, 30])
+    adjustment_values = pd.DataFrame([0.2, 0.1, 0.4, 0.3])
     num_days_per_year = 4
     num_segments_per_day = 3
 
@@ -18,20 +18,7 @@ def test_apply_daily_adjustments():
     )
 
     correct_adjustment = np.array(
-        [
-            0.1,
-            0.3,
-            0.2,
-            0.2,
-            0.4,
-            0.1,
-            0.09,
-            0.11,
-            0.1,
-            0.1,
-            0.25,
-            0.15,
-        ]
+        [0.04, 0.09, 0.07, 0.03, 0.06, 0.01, 0.14, 0.16, 0.1, 0.06, 0.15, 0.09]
     )
 
     print(adjustment_result)
@@ -41,20 +28,7 @@ def test_apply_daily_adjustments():
 
 def test_apply_annual_scaling():
     hourly_profile = np.array(
-        [
-            0.1,
-            0.3,
-            0.2,
-            0.2,
-            0.4,
-            0.1,
-            0.09,
-            0.11,
-            0.1,
-            0.1,
-            0.25,
-            0.15,
-        ]
+        [0.04, 0.09, 0.07, 0.03, 0.06, 0.01, 0.14, 0.16, 0.1, 0.06, 0.15, 0.09]
     )
     bev_vmt = 10
     charging_efficiency = 0.9
@@ -67,6 +41,6 @@ def test_apply_annual_scaling():
         kwhmi,
     )
 
-    correct_annual_scaling = np.array([20, 60, 40, 40, 80, 20, 18, 22, 20, 20, 50, 30])
+    correct_annual_scaling = np.array([8, 18, 14, 6, 12, 2, 28, 32, 20, 12, 30, 18])
 
-    assert np.array_equal(scaling_result, correct_annual_scaling)
+    np.testing.assert_almost_equal(scaling_result, correct_annual_scaling)
