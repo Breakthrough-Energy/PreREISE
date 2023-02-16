1ce180efd1deffdc0ef904f6d44f75bd15663efb
Daniel Muldrew <dmuldrew@gmail.com>
2023-02-15 22:42:10 -0800

    refactor: add mdv/hdv script

--- prereise/gather/demanddata/transportation_electrification/scripts/run_immediate_ldv_ldt.sh
+++ prereise/gather/demanddata/transportation_electrification/scripts/run_immediate_ldv_ldt.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+
 nohup python -u generate_shape_cache_files.py ldv 100  </dev/null > nohup_ldv_100.out 2> nohup_ldv_100.err &
 
 nohup python -u generate_shape_cache_files.py ldv 200  </dev/null > nohup_ldv_200.out 2> nohup_ldv_200.err &
